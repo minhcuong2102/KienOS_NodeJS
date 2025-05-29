f6614ccfccc4ed28ad639860bd767fbee2d5af9a
minhcuong2102 <108413114+minhcuong2102@users.noreply.github.com>
2025-05-29 10:39:36 +0700

    Update server.js

--- app/server.js
+++ app/server.js
@@ -54,7 +54,7 @@ app.use('/secret', express.Router().get('/', (req, res) => {
 }));
 
 app.use((req, res, next) => {
-  console.log(${req.method} ${req.url});
+  console.log(`${req.method} ${req.url}`);
   next();
 });
 
