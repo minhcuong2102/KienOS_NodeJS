e574d89e3e208ff52ef5290387706a67940294c3
minhcuong2102 <108413114+minhcuong2102@users.noreply.github.com>
2025-05-29 10:41:11 +0700

    Update server.js

--- app/server.js
+++ app/server.js
@@ -44,7 +44,7 @@ const bodyParser     = require('body-parser');
 const initApiRouter  = require('./controller/index');
 const initChatSocket = require('./socket/chat.socket'); // ⬅️ thêm
 const handleErrors   = require('./middleware/global-error.middleware');
-
+const cors = require('cors');
 const port = process.env.PORT || 8888;
 const app  = express();
 
@@ -67,6 +67,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 /* --------------------------  ROUTER CŨ  -------------------------- */
 initApiRouter(app);
 
+app.use(cors({
+  origin: ['https://kienos-frontend-z1ie.onrender.com', 'https://kienos-backend-4w2a.onrender.com'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true,
+}));
+
+app.options('*', cors());
 app.all('*', (req, res) => {
   res.send(
     'Message From Nodejs: This route doesnt exist, maybe check for the request type (GET/POST/PUT/DELETE) or the route itself'
@@ -94,5 +101,5 @@ app.set('io', io);
 
 /* --------------------------  KHỞI CHẠY  -------------------------- */
 server.listen(port, () => {
-  console.log(Server + WebSocket listening on port ${port});
+  console.log(`Server + WebSocket listening on port ${port}`);
 });
